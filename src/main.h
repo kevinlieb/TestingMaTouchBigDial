25c92e6c96a9593679573bcd508e6b55f7a43921
kevinlieb <kkevinl@yahoo.com>
2026-06-10 13:55:02 -0700

    update

---
+++ src/main.h
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+int32_t testFillScreen(void);
+int32_t testText(void);
+int32_t testPixels(void);
+
+int32_t testLines(void);
+int32_t testFastLines(void);
+int32_t testFilledRects(void);
+
+int32_t testRects(void);
+int32_t testFilledCircles(uint8_t);
+int32_t testCircles(uint8_t);
+int32_t testFillArcs(void);
+int32_t testArcs(void);
+int32_t testFilledTriangles(void);
+int32_t testTriangles(void);
+int32_t testFilledRoundRects(void);
+int32_t testRoundRects(void);
+
+void printnice(const __FlashStringHelper *, long int);
